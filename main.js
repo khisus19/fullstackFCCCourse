cd65ca577946711f2927a8c8bbb1338132067e82
Khisus19 <khisus19@gmail.com>
2022-11-07 10:13:04 -0600

    Add weapons array and README

--- main.js
+++ main.js
@@ -17,6 +17,25 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterNameText = document.querySelector("#monsterNameText");
 const monsterHealthText = document.querySelector("#monsterHealthText");
 
+const weapons = [
+  {
+    name: "stick",
+    power: 5
+  },
+  {
+    name: "dagger",
+    power: 30
+  },
+  {
+    name: "claw hammer",
+    power: 50
+  },
+  {
+    name: "sword",
+    power: 100
+  }
+];
+
 const locations = [
   {
     name: "town square",
@@ -30,6 +49,12 @@ const locations = [
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store.",
   },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters.",
+  },
 ];
 
 // Initialize buttons
@@ -41,23 +66,47 @@ function update(location) {
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+  text.innerText = location.text;
 }
 
 function goTown() {
-  update(locations[0])
+  update(locations[0]);
 }
 
 function goStore() {
- 
+  update(locations[1])
 }
 
 function goCave() {
-  console.log("Going to cave");
+  update(locations[2]);
 }
+
 function fightDragon() {
   console.log("Fight dragon");
+}
+
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health."
+  }
+}
+
+function buyWeapon() {
+  console.log("Buy weapon")
+}
+
+function fightSlime() {
+  console.log("Fight slime");
+}
+
+function fightBeast() {
+  console.log("Fight fanged beast");
 }
